e0b803de4949b32bb204bbf355b7479a02358e41
YaKaiLi <jkliyakai@163.com>
2018-04-02 23:37:56 +0800

    twoSum

---
+++ 1-twoSum/1-twoSum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            m[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); ++i) {
+            int t = target - nums[i];
+            if (m.count(t) && m[t] != i) {
+                res.push_back(i);
+                res.push_back(m[t]);
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+int main(){
+	return 0;
+} 
